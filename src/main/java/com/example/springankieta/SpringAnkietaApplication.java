9ed851ca1e9d8f4af3fb9df6359c83b12f45a9d5
fencepainter <niebieskipiesek2@gmail.com>
2023-01-26 01:37:38 +0100

    Just new Spring created

---
+++ src/main/java/com/example/springankieta/SpringAnkietaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springankieta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAnkietaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAnkietaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springankieta/SpringAnkietaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springankieta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAnkietaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
